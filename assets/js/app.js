e9579d83dcacaeffe30d839794a58dd018ccff41
Niels van Eck <niels@vaneck.nu>
2011-11-04 23:03:05 +0100

    Onnodige bestanden verwijderd & (statische) vind collega pagina gemaakt

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,11 @@
 $( function() {
-	$('#searchButton').click(function() {
-		alert('test');
-		//document.addEventListener("deviceready", onDeviceReady, false);
+	$('#collegaButton').click(function() {
+		$.mobile.changePage("#collega");
+		$("#collegaList").listview('option', 'filterCallback', filterCollegas);
 	});
 	
-	function reachableCallback(reachability) {
-		// There is no consistency on the format of reachability
-		var networkState = reachability.code || reachability;
-		var states = {};
-		states[NetworkStatus.NOT_REACHABLE]                      = 'No network connection';
-		states[NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK] = 'Carrier data connection';
-		states[NetworkStatus.REACHABLE_VIA_WIFI_NETWORK]         = 'WiFi connection';
-		alert('Connection type: ' + states[networkState]);
-	}
-	// PhoneGap is loaded and it is now safe to make calls PhoneGap methods
-	function onDeviceReady() {
-		navigator.network.isReachable('phonegap.com', reachableCallback);
-	}
+	function filterCollegas ( text, searchValue ){
+   	
+  		return text.toLowerCase().search( searchValue ) === -1;
+	};
 });
